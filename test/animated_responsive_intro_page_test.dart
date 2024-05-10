6610a6f9181aff25cc8b0a016b4239bc3440cd64
Ali Birkan Bayram <alibirkanbayram@gmail.com>
2024-05-10 23:27:23 +0300

    • Update Author

--- test/animated_responsive_intro_page_test.dart
+++ test/animated_responsive_intro_page_test.dart
@@ -1,5 +1,15 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 
-import 'package:animated_responsive_intro_page/animated_responsive_intro_page.dart';
+void main() => runApp(const MyApp());
 
-void main() {}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Animated Responsive Intro Page',
+      home: Text("Test"),
+    );
+  }
+}
