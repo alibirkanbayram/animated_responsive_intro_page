e5d3de2cb9ec621e65d2aafacf015fa69e45b476
Ali Birkan Bayram <alibirkanbayram@gmail.com>
2024-05-11 00:54:43 +0300

    • Update: Example main dart

--- lib/example/lib/main.dart
+++ lib/example/lib/main.dart
@@ -16,6 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        title: 'Animated Responsive Intro Page', home: IntroPage());
+      title: 'Animated Responsive Intro Page',
+      home: IntroPage(),
+    );
   }
 }
